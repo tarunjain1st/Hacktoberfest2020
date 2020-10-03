6470dd8762f9a410a154e1f2bae3134e1b32c087
Kevin Sheng <kevinsheng2005@gmail.com>
2020-10-03 15:26:10 -0700

    forgot to add comments

--- scripts/SansPapyrus683.java
+++ scripts/SansPapyrus683.java
@@ -1,3 +1,8 @@
+// LANGUAGE: Java
+// ENV: OpenJDK
+// AUTHOR: Kevin S
+// GITHUB: https://github.com/SansPapyrus683
+
 public class SansPapyrus683 {
     public static void main(String[] args) {
         System.out.println("hi world how's it going");
